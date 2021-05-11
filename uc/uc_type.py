f6465c531d79425c7167e3d17b09ae14b8741b2f
Tiago de Paula <tiagodepalves@gmail.com>
2021-05-11 01:39:22 -0300

    Add string representations for 'uCType'

--- uc/uc_type.py
+++ uc/uc_type.py
@@ -8,6 +8,8 @@ class uCType:
     Types are declared as singleton instances of this type.
     """
 
+    __slots__ = "name", "binary_ops", "unary_ops", "rel_ops", "assign_ops"
+
     def __init__(
         self,
         name: Optional[str],
@@ -28,6 +30,15 @@ class uCType:
     def __eq__(self, other: uCType) -> bool:
         """Primary types are only equal to themselves."""
         return self is other
+
+    def __repr__(self) -> str:
+        return f"type({self})"
+
+    def __str__(self) -> str:
+        if self.typename is None:
+            return "<unnamed>"
+        else:
+            return self.typename
 
 
 # # # # # # # # #
@@ -61,6 +72,9 @@ VoidType = uCType("void")  # no valid operation
 
 
 class ArrayType(uCType):
+    __slots__ = "type", "size"
+    typename: None
+
     def __init__(self, element_type: uCType, size: Optional[int] = None):
         """
         type: Any of the uCTypes can be used as the array's type. This
@@ -74,8 +88,14 @@ class ArrayType(uCType):
     def __eq__(self, other: uCType) -> bool:
         return isinstance(other, ArrayType) and self.type == other.type and self.size == other.size
 
+    def __str__(self) -> str:
+        return f"{self.type}[{self.size or ''}]"
+
 
 class FunctionType(uCType):
+    __slots__ = "rettype", "params"
+    typename: str
+
     def __init__(self, name: str, return_type: uCType, params: Sequence[Tuple[str, uCType]] = ()):
         """
         name: Function definition name.
@@ -83,15 +103,25 @@ class FunctionType(uCType):
         params: Sequence of 'name, type' for each of the function parameters.
         """
         super().__init__(name)  # only valid operation is call
-        self.type = return_type
+        self.rettype = return_type
         self.params = tuple(params)
 
+    @property
     def param_types(self) -> Tuple[uCType, ...]:
         return tuple(t for _, t in self.params)
 
     def __eq__(self, other: uCType) -> bool:
         return (
             isinstance(other, FunctionType)
-            and self.type == other.type
-            and self.param_types() == other.param_types()
+            and self.rettype == other.rettype
+            and self.param_types == other.param_types
         )
+
+    def __str__(self, show_names: bool = False) -> str:
+        if show_names:
+            params = ", ".join(f"{n}: {t}" for n, t in self.params)
+            return f"{self.rettype} {self.typename}({params})"
+        else:
+            # no space between parameters
+            params = ",".join(str(t) for t in self.param_types)
+            return f"{self.rettype}({params})"
